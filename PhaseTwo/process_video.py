fc5fd992e532a559fccec0ff29d317b3cfb61330
Robert Rittmuller <robert.rittmuller@dot.gov>
2021-02-12 15:40:38 -0500

    Refactored detection to include grade and ROW segmentation.

--- PhaseTwo/process_video.py
+++ PhaseTwo/process_video.py
@@ -8,7 +8,7 @@
 ## Copyright: Copyright 2020, Volpe National Transportation Systems Center
 ## Credits: <add reference to SORT & Deep SORT here>
 ## License: MIT
-## Version: 0.0.1
+## Version: 0.0.2
 ## Mmaintainer: Robert Rittmuller
 ## Email: robert.rittmuller@dot.gov
 ## Status: Active Development
@@ -34,6 +34,7 @@ from pathlib import Path
 from hurry.filesize import size, si
 from sort import *
 from deep_sort import build_tracker
+from utils import image_resize, pil_to_cv, parse_seg_prediction, instance_segmentation_visualize_sort, get_model_instance_segmentation, instance_grade_segmentation_visualize, detect_object_overlap
 
 # -------------------------------------------------------------
 # Configuration settings
@@ -53,6 +54,25 @@ force_video_fps = 0
 force_video_width = None
 force_video_height = 720
 
+# grade / right-of-way segmentation model settings
+grade_num_classes = 2
+GRADE_CATEGORY_NAMES = [
+    'GradeCrossing', 'RightOfWay'
+]
+GRADE_LABEL_COLORS = [[0, 0, 255],[0, 255, 0],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180]]
+
+# Roadway features model settings
+COCO_INSTANCE_VISIBLE_CATEGORY_NAMES = [
+    'person', 'train', 'car', 'bus', 'truck', 'motorcycle', 'bicycle'
+]
+
+# The label that is used by Deep Sort
+DEEPSORT_LABEL = 'person'
+
+# LABEL_COLORS = [[0, 255, 0],[0, 0, 255],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180],[250, 80, 190],[245, 145, 50],[70, 150, 250],[50, 190, 190]]
+# REMINDER: Label colors are Blue, Green, Red (BGR)
+LABEL_COLORS = [[0, 0, 255],[0, 255, 0],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180]]
+
 # Deep Sort Config
 class DEEPSORT_CONFIG_CLASS():
         REID_CKPT =  "./PhaseTwo/deep_sort/deep/checkpoint/ckpt.t7"
@@ -68,7 +88,7 @@ DEEPSORT_CONFIG = DEEPSORT_CONFIG_CLASS()
 # -------------------------------------------------------------
 
 # Detect if we have a GPU available
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 try:
     device
 except NameError:
@@ -92,7 +112,7 @@ if __name__ == '__main__':
     # Deal with command line arguments.
     parser = argparse.ArgumentParser(description='Process some video files using Machine Learning!')
     parser.add_argument('--outputpath', '-o',   action='store',     required=False,     default='../temp/fraoutput',        help='Path to the directory where extracted data is stored.')
-    parser.add_argument('--inputpath',  '-i',   action='store',     required=False,     default='E:/FRA/ramsey/20180418/Ch02_20180418000000_20180418235959_1a.avi',    help='Path to the extracted video frames in JPG format.')
+    parser.add_argument('--inputpath',  '-i',   action='store',     required=False,     default='/mnt/ml_data/FRA/sourcevideos/ramsey/20180418/Ch02_20180418000000_20180418235959_1e.avi',    help='Path to the extracted video frames in JPG format.')
     args = parser.parse_args()
 
     # detect platform for FFMPEG
@@ -108,63 +128,8 @@ if __name__ == '__main__':
     #     FFPROBE_PATH = default_ffprobe_path if path.exists(default_ffprobe_path) else '/usr/bin/ffprobe'
 
     # -------------------------------------------------------------
-    # Annotation Labels / Classes
-
-    COCO_INSTANCE_CATEGORY_NAMES = [
-        '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-        'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-        'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-        'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-        'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-        'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-        'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-        'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-        'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-        'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-        'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-        'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-    ]
-
-    COCO_INSTANCE_VISIBLE_CATEGORY_NAMES = [
-        'person', 'train', 'car', 'bus', 'truck', 'motorcycle', 'bicycle'
-    ]
-
-    # The label that is used by Deep Sort
-    DEEPSORT_LABEL = 'person'
-
-    # LABEL_COLORS = [[0, 255, 0],[0, 0, 255],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180],[250, 80, 190],[245, 145, 50],[70, 150, 250],[50, 190, 190]]
-    # REMINDER: Label colors are Blue, Green, Red (BGR)
-    LABEL_COLORS = [[0, 0, 255],[0, 255, 0],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180]]
-    
     # Helper functions
     # -----------------------------------------------------------------------------------
-    
-    def image_resize(org_width, org_height, width = None, height = None):
-        # initialize the dimensions of the image to be resized and
-        # grab the image size
-        dim = None
-
-        # if both the width and height are None, then return the
-        # original image
-        if width is None and height is None:
-            return org_width, org_height
-
-        # check to see if the width is None
-        if width is None:
-            # calculate the ratio of the height and construct the
-            # dimensions
-            r = height / float(org_height)
-            dim = (int(org_width * r), height)
-
-        # otherwise, the height is None
-        else:
-            # calculate the ratio of the width and construct the
-            # dimensions
-            r = width / float(org_width)
-            dim = (width, int(org_height * r))
-
-        # return the resized dimensions
-        return dim
 
     def run_model(model, data):
         with torch.no_grad():
@@ -207,123 +172,6 @@ if __name__ == '__main__':
 
     #     image_pipe = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=4*1024*1024)
 
-    def pil_to_cv(image):
-        new_image = np.array(image)
-        return new_image[:, :, ::-1].copy()
-
-    def load_labels(filePath):
-        label_file = open(filePath, 'r')
-        file_data = label_file.readlines()
-        labels = []
-        for line in file_data:
-            labelset = []
-            idx,label = line.split(':')
-            labelset.append(idx)
-            labelset.append(label)
-            labels.append(labelset)
-        return labels
-
-    def save_csv(outputpath, data):
-        with open(outputpath, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            for data_row in data:
-                csv_writer.writerow(data_row)
-
-    def get_file_list(directory, extension):
-        return (f for f in listdir(directory) if f.endswith('.' + extension))
-
-    def isodd(num):
-        mod = num % 2
-        if mod > 0:
-            return True
-        else:
-            return False
-
-    def parse_seg_prediction(pred, threshold):
-
-        pred_score = list(pred['scores'].detach().cpu().numpy())
-        pred_t = [pred_score.index(x) for x in pred_score if x>threshold][-1]
-        masks = (pred['masks']>0.5).squeeze().detach().cpu().numpy()
-        pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred['labels'].cpu().numpy())]
-        pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred['boxes'].detach().cpu().numpy())]
-        masks = masks[:pred_t+1]
-        pred_boxes = pred_boxes[:pred_t+1]
-        pred_class = pred_class[:pred_t+1]
-        pred_score = pred_score[:pred_t+1]
-
-        labels = pred_class.copy()
-        new_masks = []
-        new_class = []
-        new_boxes = []
-        n = 0
-        for label in pred_class:
-            if label in COCO_INSTANCE_VISIBLE_CATEGORY_NAMES:
-                new_masks.append(masks[n])
-                new_boxes.append(pred_boxes[n])
-                new_class.append(pred_class[n])
-            n += 1
-
-        return new_masks, new_boxes, new_class, pred_score
-
-    def colour_masks(image, color):
-        r = np.zeros_like(image).astype(np.uint8)
-        g = np.zeros_like(image).astype(np.uint8)
-        b = np.zeros_like(image).astype(np.uint8)
-        r[image == 1], g[image == 1], b[image == 1] = color
-        coloured_mask = np.stack([r, g, b], axis=2)
-        return coloured_mask
-
-    def fix_box_format(boxes):
-        new_boxes = []
-        for box in boxes:
-            line = []
-            for value in box:
-                for subvalue in value:
-                    line.append(subvalue)
-            new_boxes.append(line)
-        return np.array(new_boxes)
-
-    def instance_segmentation_visualize(img, predictions, threshold=0.5, rect_th=3, text_size=1, text_th=2):
-        masks, boxes, pred_cls = parse_seg_prediction(predictions, threshold)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        for i in range(len(masks)):
-            rgb_mask = colour_masks(masks[i], LABEL_COLORS[i])
-            img = cv2.addWeighted(img, 1, rgb_mask, 0.5, 0)
-            cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th)
-            cv2.putText(img,pred_cls[i], boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
-        return img
-
-    def instance_segmentation_visualize_sort(img, masks, boxes, pred_cls, scores, threshold=0.5, rect_th=2, text_size=.75, text_th=2, classname='Object'):
-        
-        # if there are no objects detected, we still need to notify the SORT object
-        if(len(boxes) == 0):
-            if classname == DEEPSORT_LABEL:
-                sort_boxes = []
-            else:
-                sort_boxes = sort_trackers[classname].update()
-        else:
-            if classname == DEEPSORT_LABEL:
-                sort_boxes = deep_sort_tracker.update(fix_box_format(boxes),scores,img)
-            else:
-                sort_boxes = sort_trackers[classname].update(fix_box_format(boxes))
-        
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        label_color_idx = COCO_INSTANCE_VISIBLE_CATEGORY_NAMES.index(classname)
-
-        for i in range(len(masks)):
-            rgb_mask = colour_masks(masks[i], LABEL_COLORS[label_color_idx])
-            img = cv2.addWeighted(img, 1, rgb_mask, 0.5, 0)
-
-        for i in range(len(sort_boxes)):
-            x = (int(sort_boxes[i][0]), int(sort_boxes[i][1]))
-            y = (int(sort_boxes[i][2]), int(sort_boxes[i][3]))
-            cv2.rectangle(img,x, y,color=(0, 255, 0), thickness=rect_th)
-            cv2.putText(img, str(classname) + ' #' + str(int(sort_boxes[i][4])), (int(sort_boxes[i][0]), int(sort_boxes[i][1])), cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0), thickness=text_th)
-        
-        return img
-
     # def instance_segmentation_visualize_sort(img, predictions, threshold=0.5, rect_th=1, text_size=1, text_th=1):
     #     masks, boxes, pred_cls = parse_seg_prediction(predictions, threshold)
     #     fixed_boxes = fix_box_format(boxes)
@@ -355,6 +203,12 @@ if __name__ == '__main__':
     # Load the object detection model
     model_road = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True).to(device)
     model_road.eval()
+
+    # load the grade / right-of-way model
+    grade_model = get_model_instance_segmentation(grade_num_classes)
+    grade_model.load_state_dict(torch.load('PhaseTwo/models/gctd_grade-row.pt'))
+    grade_model.eval()
+    grade_model.to(device)
 
     # load the activity classification model
     # model_class = torch.load(args.modelfile)
@@ -401,6 +255,7 @@ if __name__ == '__main__':
     print('Saving video as: ', output_filename)
     video_out = cv2.VideoWriter(output_filename, fourcc, video_output_fps, (videoSize))
 
+    grade_segmented = False
     framecount = 0
     extracted_data = []
     
@@ -428,6 +283,15 @@ if __name__ == '__main__':
         # load batch onto GPU / or register in CPU
         image_stack = image_stack.to(device)
 
+        # get the grade segmentation masks *only on the first batch*
+        if(grade_segmented == False):
+            grade_annotations = run_model(grade_model, image_stack)
+            grade_segmented = True
+
+            # upload the model since we are done with it
+            del grade_model
+        
+
         # get the model predictions / annotations
         road_annotations = run_model(model_road, image_stack)
 
@@ -454,18 +318,9 @@ if __name__ == '__main__':
             org_image1 = pil_to_cv(org_image_stack[n])
 
             # create JSON output
-            # road_output = []
-
             if ([i for i in road_annotation['scores'] if i >= segmentation_threshold]):
-                road_masks, road_boxes, road_labels, road_scores = parse_seg_prediction(road_annotation, segmentation_threshold)
-                # print(road_scores, road_labels, road_boxes)
+                road_masks, road_boxes, road_labels, road_scores = parse_seg_prediction(road_annotation, segmentation_threshold, COCO_INSTANCE_VISIBLE_CATEGORY_NAMES)
 
-                # Process predictions for SORT tracking
-                # road_img, road_sort_boxes = instance_segmentation_visualize_sort(org_image1, road_annotation, segmentation_threshold, boxes_thickness, boxes_text_size, boxes_text_thickness)
-                # road_output.append(road_scores)
-                # road_output.append(road_boxes)
-            # else:
-                # mot_tracker.update(np.empty((0, 5)))
             road_img = org_image1
 
             # fix the colors..
@@ -485,7 +340,9 @@ if __name__ == '__main__':
                         collected_labels.append(road_labels[idx])
                         collected_scores.append(road_scores[idx])
                     idx += 1
-                road_img = instance_segmentation_visualize_sort(road_img, collected_masks, collected_boxes, collected_labels, collected_scores, segmentation_threshold, classname=label)
+                road_img, grade_masks = instance_grade_segmentation_visualize(road_img, grade_annotations[0], GRADE_CATEGORY_NAMES, GRADE_LABEL_COLORS)
+                detect_object_overlap(grade_masks,GRADE_CATEGORY_NAMES,collected_masks,collected_labels)
+                road_img = instance_segmentation_visualize_sort(road_img, collected_masks, collected_boxes, collected_labels, collected_scores, COCO_INSTANCE_VISIBLE_CATEGORY_NAMES, LABEL_COLORS, DEEPSORT_LABEL, sort_trackers, deep_sort_tracker, segmentation_threshold, classname=label)
                 new_line = [collected_boxes]
                 boxes_by_label[label] = new_line
             
@@ -517,6 +374,3 @@ if __name__ == '__main__':
     mins, secs = divmod(total_time, 60)
     hours, mins = divmod(mins, 60)
     sys.stdout.write("Total running time: %d:%d:%d.\n" % (hours, mins, secs))
-
-    # For memory debugging
-    #print(torch.cuda.memory_summary())
